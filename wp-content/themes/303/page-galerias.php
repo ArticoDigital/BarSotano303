4ac6d14ba5c5bcc4700ff090444e5ca18f54cd8c
Juan2Ramos <juan2ramos@gmail.com>
2017-06-10 15:35:20 -0500

    galerias

---
+++ wp-content/themes/303/page-galerias.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+    <section class="Galleries">
+        <div id="primary" class="content-area   row ">
+            <?php
+            $terms = get_terms("categoria", array("hide_empty" => false));
+            foreach ($terms as $cat) : $nameChef = explode(" ", $cat->name) ?>
+            <figure class="col-4 small-12" data-filter=".<?php echo $cat->slug ?>">
+                <h3><?php echo $cat->name ?></h3>
+                <a href="/galeria/<?php echo $cat->slug ?>"><img src="<?php print_r(get_option("taxonomy_" . $cat->term_id)['imagen']); ?>" alt=""></a>
+                <p class="Recipes-chefsDescription"><?php print_r($cat->description) ?></p>
+                <a class="Galleries-link" href="/galeria/<?php echo $cat->slug ?>">Ver galeria <span>→</span></a>
+            </figure> <?php endforeach ?>
+
+        </div>
+    </section>
+
+<?php get_footer(); ?>
