4784839aa81d36c775eda8fbe682b6ea2421bffe
Juan2Ramos <juan2ramos@gmail.com>
2017-06-18 23:18:22 -0500

    contact

--- wp-content/themes/303/page-contacto.php
+++ wp-content/themes/303/page-contacto.php
@@ -1,21 +1,30 @@
-
-
+<?php
+/*
+Template Name: Contacto
+*/
+?>
 <?php get_header(); ?>
-<section class="ubicacion">
-    <div id="primary" class="content-area   row ">
-        sddfsdfsd
-        <?php the_content(); ?>
 
+<?php
+while (have_posts()) : the_post(); ?>
+    <section class="contacto">
+        <h3>Contáctenos</h3>
+        <p>¿Tiene algo que contarnos? aqui puedes contarnos tu experiencia, felicitarnos, proponernos y dart u punto de vista. para nosotros es muy importante saber lo que piensas.</p>
+        <?php the_content() ?>
 
-    </div>
-</section>
-
+    </section>
+<?php endwhile; ?>
 
 <script>// <![CDATA[
-    function myMap() { var uluru = {lat: 4.602988, lng: -74.068351}; var map = new google.maps.Map(document.getElementById('googleMap'), { zoom: 20, center: uluru }); var marker = new google.maps.Marker({ position: uluru, map: map,icon: 'http://303.app/wp-content/themes/303/assets/images/logo_menu.svg'
-    }); }
+    function myMap() {
+        var uluru = {lat: 4.602988, lng: -74.068351};
+        var map = new google.maps.Map(document.getElementById('googleMap'), {zoom: 20, center: uluru});
+        var marker = new google.maps.Marker({
+            position: uluru, map: map, icon: 'http://303.app/wp-content/themes/303/assets/images/logo_menu.svg'
+        });
+    }
     // ]]></script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZO_mB7TA-ejRw4QXMkU12tC5umDgsgNw&amp;callback=myMap"></script>
 
-<?php get_footer(); ?>
+<?php get_footer() ?>
