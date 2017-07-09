23d5a8d24fe3d00b9db16136cf65de26422f72ca
Daniel Quintero <danielrqo@gmail.com>
2017-07-08 21:47:30 -0500

    mapa globo

--- wp-content/themes/303/page-ubicacion.php
+++ wp-content/themes/303/page-ubicacion.php
@@ -23,8 +23,29 @@
 
 
     <script>// <![CDATA[
-function myMap() { var uluru = {lat: 4.602988, lng: -74.068351}; var map = new google.maps.Map(document.getElementById('googleMap'), { zoom: 20, center: uluru }); var marker = new google.maps.Marker({ position: uluru, map: map,icon: 'http://barsotano303.com/wp-content/themes/303/assets/images/logo_menu.svg'
- }); }
+function myMap() { 
+	var uluru = {lat: 4.602988, lng: -74.068351}; 
+	var map = new google.maps.Map(document.getElementById('googleMap'), { 
+		zoom: 18, 
+		center: uluru,
+        scrollwheel: false, 
+    }); 
+	var marker = new google.maps.Marker({ 
+
+							position: uluru, 
+							map: map,icon: '/wp-content/themes/303/assets/images/logo_menu.svg'}); 
+	marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+    var infowindow = new google.maps.InfoWindow({
+                content: '<h4 >Bar Sótano 303</h4>' +
+                '<div id="bodyContent">' +
+                '<p>Calle 19 No 3-03, Centro de Bogotá</p>' +
+                '</div>'
+            });
+	}
+
+          
 // ]]></script>
 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZO_mB7TA-ejRw4QXMkU12tC5umDgsgNw&amp;callback=myMap"></script>
